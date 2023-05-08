99ebe3f61246ae622948ea59a0e6d189804cfbce
techdave <klatzdavie19@gmail.com>
2023-05-08 12:22:05 +0300

    implement pagination for events

--- frontend/pages/events/index.js
+++ frontend/pages/events/index.js
@@ -1,8 +1,12 @@
 import EventItem from '../../components/EventItem';
 import Layout from '../../components/Layout';
 import { API_URL } from '../../config';
+import Pagination from '../../components/Pagination';
+import { PER_PAGE } from '../../config';
 
-export default function EventsPage({ events }) {
+export default function EventsPage({ events, page, total }) {
+  let lastPage = Math.ceil(total / PER_PAGE);
+
   return (
     <Layout title='all events'>
       <h1>Events</h1>
@@ -11,14 +15,24 @@ export default function EventsPage({ events }) {
       {events.map((evt) => (
         <EventItem key={evt.id} id={evt.id} evt={evt.attributes || evt} />
       ))}
+
+      <Pagination total={total} page={page} />
     </Layout>
   );
 }
 
-export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/api/events?populate=image`);
+export async function getServerSideProps({ query: { page = 1 } }) {
+  const res = await fetch(
+    `${API_URL}/api/events?populate=image&sort=date:asc&pagination[page]=${page}&pagination[pageSize]=${PER_PAGE}`
+  );
   let events = await res.json();
-  events = events.data;
 
-  return { props: { events }, revalidate: 1 };
+  return {
+    props: {
+      events: events.data,
+      pagination: events.meta.pagination,
+      page: events?.meta?.pagination?.page || null,
+      total: events?.meta?.pagination?.total || null,
+    },
+  };
 }
